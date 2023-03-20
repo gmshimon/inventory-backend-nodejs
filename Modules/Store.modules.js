349ed2c52a1916e47073c5b051c619c9e9845c1f
GMShimon <simon.rosedale99@gmail.com>
2023-03-21 03:00:05 +0800

    store module done

---
+++ Modules/Store.modules.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const validator = require("validator");
+const ObjectId = require("validator");
+
+const StoreSchema = mongoose.Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:[true,"Please provide Store name"],
+        lowercase:true,
+        unique:true,
+        enum: {
+            values: ["dhaka", "chattogram", "rajshahi", "sylhet", "khulna", "barishal", "rangpur", "mymensingh"],
+            message: "{VALUE} is not a valid name"
+          }
+    },
+    description:String,
+    status:{
+        type:String,
+        enum:["active","in-active"],
+        default:"active"
+    },
+    manager: {
+        name: String,
+        contactNumber: String,
+        id: {
+          type: ObjectId,
+          ref: 'User'
+        }
+    }
+},{
+    timestamps:true
+})
+
+const Store = mongoose.model("Store",StoreSchema);
+
+module.exports = Store;
