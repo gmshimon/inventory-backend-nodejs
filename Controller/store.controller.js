fbe91d835a160f1b388244aac059e1205809cb26
GMShimon <simon.rosedale99@gmail.com>
2023-03-24 05:24:15 +0800

    store route done

--- Controller/store.controller.js
+++ Controller/store.controller.js
@@ -1,10 +1,51 @@
+const Store = require('../Modules/Store.modules')
+
 module.exports.postStore = async(req,res,next)=>{
     try {
-        res.send("Api hit successfully")
+        const result = await Store.create(req.body)
+        res.status(200).json({
+            status:"Success",
+            message:"Data inserted successfully",
+            data:result
+        })
     } catch (error) {
         res.status(400).json({
             status:'Fail',
             message:"Couldn't insert store data"
         })
+    }
+}
+
+module.exports.getStore = async(req,res,next)=>{
+    try {
+        const result = await Store.find({})
+        res.status(200).json({
+            status:"Success",
+            message:"Data inserted successfully",
+            data:result
+        })
+    } catch (error) {
+        res.status(400).json({
+            status:'Fail',
+            message:"Couldn't fetch data"
+        }) 
+    }
+}
+
+module.exports.getStoreDetails = async(req,res,next)=>{
+    try {
+       const {id} = req.params;
+       const result = await Store.findById(id);
+
+       res.status(200).json({
+            status:"Success",
+            message:"Data inserted successfully",
+            data:result
+        })
+    } catch (error) {
+        res.status(400).json({
+            status:'Fail',
+            message:"Couldn't fetch data"
+        }) 
     }
 }
--- routes/store.route.js
+++ routes/store.route.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../Controller/store.controller');
 
-router.route('/').post(storeController.postStore)
+router.route('/')
+.post(storeController.postStore)
+.get(storeController.getStore)
+
+router.route('/:id').get(storeController.getStoreDetails)
 
 module.exports = router
