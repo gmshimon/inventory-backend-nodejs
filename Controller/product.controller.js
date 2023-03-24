c98c16743361946167b08415baaff3534c453284
GMShimon <simon.rosedale99@gmail.com>
2023-03-24 15:49:11 +0800

    product route done

--- Controller/product.controller.js
+++ Controller/product.controller.js
@@ -10,3 +10,20 @@ module.exports.postProduct = async(req,res,next)=>{
         })
     }
 }
+
+module.exports.getProducts = async(req,res,next)=>{
+    try {
+        const result = await Product.find({});
+
+        res.status(200).json({
+            status:"Success",
+            message:"Data successfully fetched",
+            data:result
+        })
+    } catch (error) {
+        res.status(400).json({
+            status:"Fail",
+            message:"Fail to insert Product"
+        })
+    }
+}
--- routes/product.route.js
+++ routes/product.route.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router();
 const productController = require('../Controller/product.controller');
 
-router.route('/').post(productController.postProduct)
+router.route('/')
+.post(productController.postProduct)
+.get(productController.getProducts)
 
 module.exports = router;
