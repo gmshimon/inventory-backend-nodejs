2c5449c09ae42c5e76d69640f1d7841613daa555
GMShimon <simon.rosedale99@gmail.com>
2023-03-26 00:35:49 +0800

    supplier post route

--- Controller/supplier.controller.js
+++ Controller/supplier.controller.js
@@ -1,5 +1,7 @@
+const Supplier = require('../Modules/Supplier.modules')
 module.exports.postSupplier = async(req,res,next)=>{
     try {
+        const result = await Supplier.create(req.body)
         res.status(200).json({
             status:"Success",
             message:"data inserted successfully"
